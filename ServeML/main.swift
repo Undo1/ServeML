648278d9c794d33cfde81403f2b131f5c75fbe95
Parker Erway <parkererway@parkers-macbook-pro-2.local>
2018-07-15 21:51:57 -0600

    Initial Commit

---
+++ ServeML/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ServeML
+//
+//  Created by Parker Erway on 7/15/18.
+//  Copyright © 2018 Parker Erway. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
