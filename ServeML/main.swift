714ae216f1661a3e7116a9fd9cf8abc233cc1b2f
Undo1 <parker@erwaysoftware.com>
2018-07-17 10:37:13 -0600

    Initial commit

--- ServeML/main.swift
+++ ServeML/main.swift
@@ -7,6 +7,46 @@
 //
 
 import Foundation
+import NaturalLanguage
+import CoreML
 
-print("Hello, World!")
-
+do {
+    let path = Bundle.main.path(forResource: "RecQuestionClassifier", ofType: "mlmodel")!
+    let url = URL(string: path)!
+    let classifier = try! NLModel(contentsOf: MLModel.compileModel(at: url));
+    
+    let server = HttpServer()
+    server["/"] = { request in
+        let bodyParams = request.queryParams.filter({ (arg0) -> Bool in
+            let (key, _) = arg0
+            return key == "body";
+        });
+        
+        if bodyParams.count == 1
+        {
+            let body = bodyParams[0]
+            
+            print(body)
+            
+            if let prediction: String = classifier.predictedLabel(for: body.1) {
+                print(prediction)
+                return .ok(.json([ "prediction": prediction ] as AnyObject))
+            }
+            else {
+                print("Not-rec")
+                return .ok(.json([ "prediction": "" ] as AnyObject))
+            }
+        }
+        
+        return .badRequest(nil)
+    }
+    
+    try server.start(6543)
+    
+    print("Server has started ( port = \(try server.port()) ). Try to connect now...")
+    
+    RunLoop.main.run()
+    
+} catch {
+    print("Server start error: \(error)")
+}
