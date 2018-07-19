a94143fc1b270a5943ac7926f398d97cf74169ce
Undo1 <parker@erwaysoftware.com>
2018-07-18 21:33:16 -0600

    Add hosting for SmellDetector model

--- ServeML/main.swift
+++ ServeML/main.swift
@@ -11,24 +11,56 @@ import NaturalLanguage
 import CoreML
 
 do {
-    let path = Bundle.main.path(forResource: "RecQuestionClassifier", ofType: "mlmodel")!
-    let url = URL(string: path)!
-    let classifier = try! NLModel(contentsOf: MLModel.compileModel(at: url));
-    
+    let reqClassifierPath = Bundle.main.path(forResource: "RecQuestionClassifier", ofType: "mlmodel")!
+    let reqClassifierUrl = URL(string: reqClassifierPath)!
+    let reqQuestionClassifier = try! NLModel(contentsOf: MLModel.compileModel(at: reqClassifierUrl));
+
+    let commentClassifierPath = Bundle.main.path(forResource: "SmellDetectorComments", ofType: "mlmodel")!
+    let commentClassifierUrl = URL(string: commentClassifierPath)!
+    let commentClassifier = try! NLModel(contentsOf: MLModel.compileModel(at: commentClassifierUrl))
+
     let server = HttpServer()
+
+    server["/comments"] = { request in
+        let bodyParams = request.queryParams.filter({ (arg0) -> Bool in
+            let (key, _) = arg0
+            return key == "body"
+        })
+
+        if bodyParams.count == 1
+        {
+            let body = bodyParams[0]
+
+            print("comment")
+            print(body)
+
+            if let prediction: String = commentClassifier.predictedLabel(for: body.1) {
+                print(prediction)
+                return .ok(.json([ "prediction": prediction ] as AnyObject))
+            }
+            else {
+                print("Not-rec")
+                return .ok(.json([ "prediction": "" ] as AnyObject))
+            }
+        }
+
+        return .badRequest(nil)
+    }
+
     server["/"] = { request in
         let bodyParams = request.queryParams.filter({ (arg0) -> Bool in
             let (key, _) = arg0
-            return key == "body";
-        });
+            return key == "body"
+        })
         
         if bodyParams.count == 1
         {
             let body = bodyParams[0]
             
+            print("post")
             print(body)
             
-            if let prediction: String = classifier.predictedLabel(for: body.1) {
+            if let prediction: String = reqQuestionClassifier.predictedLabel(for: body.1) {
                 print(prediction)
                 return .ok(.json([ "prediction": prediction ] as AnyObject))
             }
